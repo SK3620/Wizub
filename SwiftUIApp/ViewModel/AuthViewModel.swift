c51a7d5f1e46c65b15e2a385fc1dd8360d9cd67d
SK3620 <k.n.t11193620@gmail.com>
2024-08-11 19:53:37 +0900

    Add: "email"への空文字＆非Eメール形式時のベリデーションを追加

--- SwiftUIApp/ViewModel/AuthViewModel.swift
+++ SwiftUIApp/ViewModel/AuthViewModel.swift
@@ -35,6 +35,19 @@ class AuthViewModel: ObservableObject {
             .eraseToAnyPublisher()
     }
     
+    private var emailRequiredPublisher: AnyPublisher<(email: String, isValid: Bool), Never> {
+        return $email
+            .map { (email: $0, isValid: !$0.isEmpty) }
+            .eraseToAnyPublisher()
+    }
+    
+    private var emailValidPublisher: AnyPublisher<(email: String, isValid: Bool), Never> {
+        return emailRequiredPublisher
+            .filter { $0.isValid }
+            .map { (email: $0.email, isValid: $0.email.isValidEmail())}
+            .eraseToAnyPublisher()
+    }
+    
     init(){
         userNameValidPublisher
             .receive(on: RunLoop.main)
@@ -42,5 +55,28 @@ class AuthViewModel: ObservableObject {
             .map { $0 ? "" : "Username is missing"}
             .assign(to: \.usernameError, on: self)
             .store(in: &cancellableBag)
+        
+        emailRequiredPublisher
+            .receive(on: RunLoop.main)
+            .dropFirst()
+            .map { $0.isValid ? "" : "Email is Missing"}
+            .assign(to: \.emailError, on: self)
+            .store(in: &cancellableBag)
+        
+        emailValidPublisher
+            .receive(on: RunLoop.main)
+            .map { $0.isValid ? "" : "Email is not valid"}
+            .assign(to: \.emailError, on: self)
+            .store(in: &cancellableBag)
+    }
+}
+
+extension String {
+    
+    // emailの形式がどうか
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: self)
     }
 }
