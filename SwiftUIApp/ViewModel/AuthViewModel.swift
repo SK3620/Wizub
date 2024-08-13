1f03b508b42a5379ae74648a5ec975223b12dd77
SK3620 <k.n.t11193620@gmail.com>
2024-08-13 14:22:26 +0900

    Update: 各Publisherの型を変更し、バリデーションによるエラーメッセージを発行するように調整

--- SwiftUIApp/ViewModel/AuthViewModel.swift
+++ SwiftUIApp/ViewModel/AuthViewModel.swift
@@ -9,13 +9,13 @@ import Foundation
 import Combine
 
 class AuthViewModel: ObservableObject {
-        
+    
     private var cancellableBag = Set<AnyCancellable>()
     
-    // 初期値はログイン画面
     @Published var segmentType: SegmentType = .signInSegment
     
     @Published var userName: String = ""
+    
     @Published var usernameError: String = ""
     
     @Published var email: String = ""
@@ -24,139 +24,79 @@ class AuthViewModel: ObservableObject {
     @Published var password: String = ""
     @Published var passwordError: String = ""
     
-    @Published var confirmPassword: String = ""
-    @Published var confirmPasswordError: String = ""
+    @Published var enableSignUp: Bool = false
     
-    private var userNameValidPublisher: AnyPublisher<Bool, Never> {
+    private var usernameValidPublisher: AnyPublisher<ErrorMessages, Never> {
         return $userName
             .map {
-                !$0.isEmpty
+               !$0.isEmpty ? ErrorMessages.noError : ErrorMessages.missingUsername
             }
             .eraseToAnyPublisher()
     }
     
-    private var emailRequiredPublisher: AnyPublisher<(email: String, isValid: Bool), Never> {
+    private var emailValidPublisher: AnyPublisher<ErrorMessages, Never> {
         return $email
-            .map { (email: $0, isValid: !$0.isEmpty) }
-            .eraseToAnyPublisher()
-    }
-    
-    private var emailValidPublisher: AnyPublisher<(email: String, isValid: Bool), Never> {
-        return emailRequiredPublisher
-            .filter { $0.isValid }
-            .map { (email: $0.email, isValid: $0.email.isValidEmail())}
-            .eraseToAnyPublisher()
-    }
-    
-    private var emailServerValidPublisher: AnyPublisher<Bool, Never>{
-        return emailValidPublisher
-            .filter { $0.isValid }
-            .map { $0.email }
-            .debounce(for: 0.5, scheduler: RunLoop.main)
-            .removeDuplicates()
-            .map { _ in 
-                print("サーバーとの通信を行い、emailの重複がないか確認するメソッドを実行")
-                return true // 暫定でtrue
+            .map {
+                switch true {
+                case $0.isEmpty:
+                    return ErrorMessages.missingEmail
+                case !$0.isValidEmail():
+                    return ErrorMessages.invalidEmail
+                default:
+                    return ErrorMessages.noError
+                }
             }
             .eraseToAnyPublisher()
     }
     
-    private var passwordRequiredPublisher: AnyPublisher<(password: String, isValid: Bool), Never> {
+    private var passwordValidPublisher: AnyPublisher<ErrorMessages, Never> {
         return $password
-            .map { (password: $0, isValid: !$0.isEmpty) }
-            .eraseToAnyPublisher()
-    }
-        
-    private var passwordValidPublisher: AnyPublisher<Bool, Never> {
-        return passwordRequiredPublisher
-            .filter { $0.isValid }
-            .map { $0.password.isValidPassword() }
-            .eraseToAnyPublisher()
-    }
-    
-    private var confirmPasswordRequiredPublisher: AnyPublisher<(password: String, isValid: Bool), Never> {
-        return $confirmPassword
-            .map { (password: $0, isValid: !$0.isEmpty) }
-            .eraseToAnyPublisher()
-    }
-    
-    private var passwordEqualPublisher: AnyPublisher<Bool, Never> {
-        return Publishers.CombineLatest($password, $confirmPassword)
-            .filter { !$0.0.isEmpty && !$0.1.isEmpty}
-            .map { password, confirm in
-                return password == confirm
+            .map {
+                switch true {
+                case $0.isEmpty:
+                    return ErrorMessages.missingPassword
+                case !$0.isValidPassword():
+                    return ErrorMessages.invalidPassword
+                default:
+                    return ErrorMessages.noError
+                }
             }
             .eraseToAnyPublisher()
     }
     
-    init(){
-        userNameValidPublisher
-            .receive(on: RunLoop.main)
-            .dropFirst()
-            .map { $0 ? "" : "Username is missing"}
-            .assign(to: \.usernameError, on: self)
-            .store(in: &cancellableBag)
+    init() {
         
-        emailRequiredPublisher
+        usernameValidPublisher
             .receive(on: RunLoop.main)
             .dropFirst()
-            .map { $0.isValid ? "" : "Email is Missing"}
-            .assign(to: \.emailError, on: self)
+            .map { return $0.rawValue }
+            .assign(to: \.usernameError, on: self)
             .store(in: &cancellableBag)
         
         emailValidPublisher
             .receive(on: RunLoop.main)
-            .map { $0.isValid ? "" : "Email is not valid"}
-            .assign(to: \.emailError, on: self)
-            .store(in: &cancellableBag)
-        
-        emailServerValidPublisher
-            .receive(on: RunLoop.main)
-            .map { $0 ? "" : "Email is already used"}
-            .assign(to: \.emailError, on: self)
-            .store(in: &cancellableBag)
-        
-        passwordRequiredPublisher
-            .receive(on: RunLoop.main)
             .dropFirst()
-            .map { $0.isValid ? "" : "Password is missing" }
-            .assign(to: \.passwordError, on: self)
+            .map { $0.rawValue }
+            .assign(to: \.emailError, on: self)
             .store(in: &cancellableBag)
         
         passwordValidPublisher
             .receive(on: RunLoop.main)
-            .map { $0 ? "" : "8-16 chars, letters & numbers"}
+            .dropFirst()
+            .map { return $0.rawValue }
             .assign(to: \.passwordError, on: self)
             .store(in: &cancellableBag)
         
-        confirmPasswordRequiredPublisher
+        Publishers.CombineLatest3(usernameValidPublisher, emailValidPublisher, passwordValidPublisher)
             .receive(on: RunLoop.main)
-            .dropFirst()
-            .map { $0.isValid ? "" : "Confirm Password is missing" }
-            .assign(to: \.confirmPasswordError, on: self)
-            .store(in: &cancellableBag)
-        
-        passwordEqualPublisher
-            .receive(on: RunLoop.main)
-            .dropFirst()
-            .map { $0 ? "" : "Passwords dose not match" }
-            .assign(to: \.confirmPasswordError, on: self)
+            .map { userName, email, password in
+                return [userName, email, password].allSatisfy { $0 == .noError }
+            }
+            .assign(to: \.enableSignUp, on: self)
             .store(in: &cancellableBag)
     }
-}
-
-extension String {
     
-    // emailの形式がどうか
-    func isValidEmail() -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
-        return emailPred.evaluate(with: self)
-    }
-    
-    // 英字と数字を含む8〜16文字数のパスワード
-    func isValidPassword(pattern: String = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,16}$") -> Bool {
-        let passwordRegex = pattern
-        return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: self)
+    deinit {
+        cancellableBag.removeAll()
     }
 }
