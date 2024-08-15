902d7839eb665eafc36120258a57b1e9b6f7d7b9
SK3620 <k.n.t11193620@gmail.com>
2024-08-15 17:55:40 +0900

    Add: Emailの重複チェック用に"isDuplicatedEmail'キーを追加

--- SwiftUIApp/Model/AuthModel.swift
+++ SwiftUIApp/Model/AuthModel.swift
@@ -13,44 +13,21 @@ struct AuthModel: Codable {
     let email: String
     let password: String
     let apiToken: String
+    let isDuplicatedEmail: Bool?
     
-    init(name: String, email: String, password: String, apiToken: String) {
+    init(name: String, email: String, password: String, apiToken: String, isDuplicatedEmail: Bool?) {
         self.name = name
         self.email = email
         self.password = password
         self.apiToken = apiToken
+        self.isDuplicatedEmail = isDuplicatedEmail
     }
     
     enum CodingKeys: String, CodingKey {
-        case name = "name"
-        case email = "email"
-        case password = "password"
+        case name
+        case email
+        case password
         case apiToken = "api_token"
-    }
-}
-
-
-struct AuthModel2: Codable, Identifiable {
-    
-    let id: Int
-    let name: String
-    let email: String
-    let password: String
-    let apiToken: String
-    
-    init(id: Int, name: String, email: String, password: String, apiToken: String) {
-        self.id = id
-        self.name = name
-        self.email = email
-        self.password = password
-        self.apiToken = apiToken
-    }
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case name = "name"
-        case email = "email"
-        case password = "password"
-        case apiToken = "api_token"
+        case isDuplicatedEmail = "is_duplicated_email"
     }
 }
