9c08d8a49f03d675206ae737b3fc228c942a6521
SK3620 <k.n.t11193620@gmail.com>
2024-08-19 20:33:18 +0900

    Add: カスタム検索バー、カスタムボトムタブバー、ナビゲーションバーを一つの画面に表示

---
+++ SwiftUIApp/View/HomeView.swift
@@ -0,0 +1,56 @@
+//
+//  HomeView.swift
+//  SwiftUIApp
+//
+//  Created by 鈴木 健太 on 2024/08/19.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State var currentTab: Tab = .videoList
+    @State var text: String = ""
+    
+    init() {
+        // デフォルトのTabBarは使用しないので隠しておく
+        UITabBar.appearance().isHidden = true
+    }
+    
+    var body: some View {
+        ZStack {
+            // 背景色
+            Color.gray.opacity(0.1).ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                
+                SearchBarView(text: $text, placeHodler: "Search")
+                    .padding([.horizontal, .bottom])
+                
+                TabView(selection: $currentTab) {
+                    
+                    NavigationLink(NavigationPath.videoStudy.toString, value: NavigationPath.videoStudy)
+                        .tag(Tab.videoList)
+                    Text("履歴")
+                        .tag(Tab.history)
+                }
+                // ナビゲーションバーに要素を配置
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink(value: Path.account) {
+                            Image(systemName: "person")
+                                .fontWeight(.bold)
+                        }
+                    }
+                }
+                .navigationTitle(currentTab.tabTitle())
+                
+                CustomTabBar(currentTab: $currentTab)
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
