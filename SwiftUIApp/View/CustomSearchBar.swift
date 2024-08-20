7380ebde25356838aa231ffe236a0a6a26ae2e72
SK3620 <k.n.t11193620@gmail.com>
2024-08-20 20:39:48 +0900

    Add: カスタムな検索バーを作成/SwiftUI-Introspectライブラリを使用して、textFieldのreturnTypeに.serachを適用

--- SwiftUIApp/View/CustomSearchBar.swift
+++ SwiftUIApp/View/CustomSearchBar.swift
@@ -7,66 +7,60 @@
 
 import SwiftUI
 import UIKit
+import SwiftUIIntrospect
 
-struct SearchBarUIView: UIViewRepresentable {
+struct CustomSearchBar: View {
+
     @Binding var text: String
-    let placeHolder: String?
-    init(text: Binding<String>, placeHolder: String? = nil) {
-        self._text = text
-        self.placeHolder = placeHolder
-    }
-    func makeCoordinator() -> Coordinator {
-        Coordinator(text: $text)
-    }
-    
-    func makeUIView(context: Context) -> UISearchBar {
-        let searchBar = UISearchBar()
-        searchBar.searchBarStyle = .minimal
-        searchBar.searchTextField.backgroundColor = .gray.withAlphaComponent(0.2)
+    @State private var isEditing = false
 
-        searchBar.delegate = context.coordinator
-        if let placeHolder = self.placeHolder {
-            searchBar.placeholder = placeHolder
-        }
-        return searchBar
-    }
-    func updateUIView(_ uiView: UISearchBar, context: Context) {
-        
-    }
-    class Coordinator: NSObject, UISearchBarDelegate {
-        @Binding var text: String
-        public init(text: Binding<String>) {
-            self._text = text
-        }
-        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-            self.text = searchText
-        }
-        func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-            searchBar.showsCancelButton = false
-        }
-        func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-            searchBar.showsCancelButton = true
-        }
-        func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-            searchBar.showsCancelButton = false
-            searchBar.searchTextField.endEditing(true)
-            self.text = ""
-            searchBar.searchTextField.text = ""
-        }
-    }
-}
+    var onSearchButtonClick: () -> Void
 
-struct SearchBarView: View {
-    @Binding var text: String
-    public let placeHodler: String?
     var body: some View {
-        SearchBarUIView(text: $text, placeHolder: placeHodler)
+        ZStack(alignment: .leading) {
+            HStack {
+
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(Color(.darkGray))
+                    .padding(.leading, 10)
+
+                TextField("Search", text: $text,
+                          onCommit: {
+                    guard !text.isEmpty else { return }
+                            self.onSearchButtonClick()
+                          }
+                )
+                .introspect(.textField, on: .iOS(.v13, .v14, .v15, .v16, .v17, .v18), customize: { textField in
+                    textField.returnKeyType = .search
+                    textField.attributedPlaceholder = NSAttributedString(
+                        string: "Search",
+                        attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray] // 任意の色を指定
+                    )
+                })
+                .padding(.top, 10)
+                .padding(.bottom, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+              
+                if isEditing {
+                    Button(action: {
+                        self.text = ""
+                    }, label: {
+                        Image(systemName: "multiply.circle.fill")
+                            .foregroundColor(.gray)
+                            .offset(x: -10)
+                    })
+                }
+
+            }
+        }
+        .background(Color(.gray.withAlphaComponent(0.25)))
+        .cornerRadius(16)
+    }
+
+    public func onSearchButtonClick(action: () -> Void) -> some View {
+        action()
+        return self
     }
 }
-
-struct SearchBarView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchBarView(text: .constant(""), placeHodler: nil)
-    }
-}
-
