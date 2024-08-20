fcd9121ebb65d7288cd2f5f3ededf2d71c1bf1a3
SK3620 <k.n.t11193620@gmail.com>
2024-08-20 20:44:10 +0900

    Add: YouTubeDataAPIV3のベースURLを追加/解説コメントアウトの追加

--- SwiftUIApp/Service/ApiUrl.swift
+++ SwiftUIApp/Service/ApiUrl.swift
@@ -14,9 +14,23 @@ struct ApiUrl {
         return "http〜〜"
     }
     
+    // YouTubeDataAPI V3
+    static var youTubeBaseUrl: String {
+        #if DEVELOP
+        return "https://www.googleapis.com/youtube/v3/search"
+        #endif
+        return "http〜〜"
+    }
+    
+    // サインアップ post
     static let signUpUrl = "/api/sign_up"
 
+    // サインイン post
     static let signInUrl = "/api/sign_in"
     
+    // Email重複チェック post
     static let checkEmailUrl = "api/check_email"
+
+    // なし
+    static let none = ""
 }
