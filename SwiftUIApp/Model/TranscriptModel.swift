344fb9cac3ced7e388beb8518497d979a4ce393c
SK3620 <k.n.t11193620@gmail.com>
2024-08-23 15:27:06 +0900

    Add: バックエンドから取得した動画の字幕情報用のモデルオブジェクトを作成

--- SwiftUIApp/Model/TranscriptModel.swift
+++ SwiftUIApp/Model/TranscriptModel.swift
@@ -7,23 +7,24 @@
 
 import Foundation
 
-struct TranscriptDetailModel: Codable {
-    let text: String
-    let start: Double
-    let duration: Double
-    
-    enum CodingKeys: String, CodingKey {
-        case text
-        case start
-        case duration
-    }
-}
-
 struct TranscriptModel: Codable {
     let transcripts: [TranscriptDetailModel]
     
     enum CodingKeys: String, CodingKey {
         case transcripts
+    }
+    
+    struct TranscriptDetailModel: Codable, Identifiable {
+        let id = UUID()
+        let text: String // 字幕
+        let start: Double // 字幕表示開始時間
+        let duration: Double // 字幕が表示されている時間の長さ
+        
+        enum CodingKeys: String, CodingKey {
+            case text
+            case start
+            case duration
+        }
     }
     
     // Request.Response（Decodable)型をSelfへダウンキャスト
