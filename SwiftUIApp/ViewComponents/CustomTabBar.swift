f9f67c28820123e1c3dd7f4d43e82aaecabce663
SK3620 <k.n.t11193620@gmail.com>
2024-08-23 15:30:10 +0900

    Update: タブバーの高さ、画像のサイズを調整

--- SwiftUIApp/ViewComponents/CustomTabBar.swift
+++ SwiftUIApp/ViewComponents/CustomTabBar.swift
@@ -37,33 +37,27 @@ struct CustomTabBar: View {
     @Binding var currentTab: Tab
     
     var body: some View {
-        GeometryReader { proxy in
-            HStack(spacing: 0) {
-                ForEach(Tab.allCases, id: \.hashValue) { tab in
-                    VStack {
-                        Button {
-                            currentTab = tab
-                        } label: {
-                            Image(systemName: tab.symbolName())
-                                .renderingMode(.template)
-                                .resizable()
-                                .font(.system(size: 20, weight: .bold))
-                                .frame(width: 25, height: 25)
-                                .frame(maxWidth: .infinity)
-                                .foregroundColor(currentTab == tab ? .white : .gray)
-                        }
-                        
-                        Text(tab.tabTitle())
-                            .font(.system(size: 14, weight: .bold))
+        HStack {
+            ForEach(Tab.allCases, id: \.hashValue) { tab in
+                VStack {
+                    Button {
+                        currentTab = tab
+                    } label: {
+                        Image(systemName: tab.symbolName())
+                            .font(.system(size: 30))
+                            .frame(maxWidth: .infinity)
                             .foregroundColor(currentTab == tab ? .white : .gray)
                     }
+                    
+                    // 暫定でコメントアウト中
+//                    Text(tab.tabTitle())
+//                        .font(.system(size: 14, weight: .bold))
+//                        .foregroundColor(currentTab == tab ? .white : .gray)
                 }
             }
-            .frame(maxWidth: .infinity)
         }
-        .frame(height: 30)
-        .padding(.bottom, 10)
-        .padding([.horizontal, .top])
+        .frame(maxWidth: .infinity)
+        .frame(height: 49)
         .background(ColorCodes.primary.color())
     }
 }
