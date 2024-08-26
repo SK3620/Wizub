38252bcf3904542108525aebe71dfddc9d331cac
SK3620 <k.n.t11193620@gmail.com>
2024-08-27 00:09:12 +0900

    Update: 字幕表示モードが.hideAllの場合、画面に"字幕表示中"の文言を表示

--- SwiftUIApp/View/StudyView.swift
+++ SwiftUIApp/View/StudyView.swift
@@ -21,27 +21,42 @@ struct StudyView: View {
             PlayVideoView(studyViewModel: studyViewModel)
             
             ScrollViewReader { proxy in
-                List {
-                    ForEach(Array(studyViewModel.transcriptDetail.enumerated()), id: \.offset){
-                        index, transcript in
-                        TranscriptListView(
-                            transcriptDetailModel: transcript,
-                            isHighlighted: studyViewModel.currentTranscriptIndex == index
-                        )
-                        .onTapGesture {
-                            studyViewModel.seekToTranscript(at: index)
-                            // ハイライトされるtranscriptを更新
-                            studyViewModel.currentTranscriptIndex = index
+                if studyViewModel.transcriptDisplayMode != .hideAll {
+                    List {
+                        ForEach(Array(studyViewModel.transcriptDetail.enumerated()), id: \.offset){
+                            index, transcript in
+                            TranscriptListView(
+                                transcriptDetailModel: transcript,
+                                isHighlighted: studyViewModel.currentTranscriptIndex == index, displayMode: studyViewModel.transcriptDisplayMode
+                            )
+                            .onTapGesture {
+                                // ハイライトされるtranscriptを更新
+                                studyViewModel.currentTranscriptIndex = index
+                                studyViewModel.seekToTranscript(at: index)
+                            }
                         }
                     }
-                }
-                // transcriptのindexの変更を監視
-                .onChange(of: studyViewModel.currentTranscriptIndex, { oldIndex, newIndex in
-                    if let newIndex = newIndex {
-                        withAnimation {
-                            // 指定のtranscriptへ自動スクロール
-                            proxy.scrollTo(studyViewModel.transcriptDetail[newIndex].id, anchor: .top)
+                    // transcriptのindexの変更を監視
+                    .onChange(of: studyViewModel.currentTranscriptIndex, { oldIndex, newIndex in
+                        if let newIndex = newIndex {
+                            withAnimation {
+                                // 指定のtranscriptへ自動スクロール
+                                proxy.scrollTo(studyViewModel.transcriptDetail[newIndex].id, anchor: .top)
+                            }
                         }
+                    })
+                    // 暫定で以下の動画IDの字幕取得
+                    .onAppear {
+                        studyViewModel.getTranscripts(videoId: "NaqB5zYGEsw")
+                    }
+                } else {
+                    // 字幕表示モードが.hideAllの場合
+                    VStack {
+                        Spacer()
+                        Text("字幕非表示中")
+                            .foregroundColor(Color(white: 0.5))
+                            .font(.headline)
+                        Spacer()
                     }
                 }
             }
