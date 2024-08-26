a917c241373c1cb45e9366702d0d3517d43eb229
SK3620 <k.n.t11193620@gmail.com>
2024-08-26 23:47:24 +0900

    Add: 条件分岐を用いて、それぞれの字幕表示モードの種類に応じた処理を追記

--- SwiftUIApp/ViewComponents/TranscriptListView.swift
+++ SwiftUIApp/ViewComponents/TranscriptListView.swift
@@ -10,25 +10,35 @@ import SwiftUI
 struct TranscriptListView: View {
     
     private var transcriptDetailModel: TranscriptModel.TranscriptDetailModel
-    var isHighlighted: Bool // 現在の字幕がハイライトされているかどうか
-
     
-    init(transcriptDetailModel: TranscriptModel.TranscriptDetailModel, isHighlighted: Bool){
+    // 現在の字幕がハイライトされているかどうか
+    var isHighlighted: Bool
+    
+    // 字幕表示モード
+    var displayMode: TranscriptDisplayMode
+    
+    init(transcriptDetailModel: TranscriptModel.TranscriptDetailModel, isHighlighted: Bool, displayMode: TranscriptDisplayMode){
         self.transcriptDetailModel = transcriptDetailModel
         self.isHighlighted = isHighlighted
+        self.displayMode = displayMode
     }
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text(transcriptDetailModel.text)
-                .font(.body)
-                .foregroundColor(isHighlighted ? .red : .primary) // ハイライトされた字幕は赤色
-            HStack {
-                Text("開始時間: \(transcriptDetailModel.start)秒")
-                Text("持続時間: \(transcriptDetailModel.duration)秒")
+            if displayMode != .hideEnglish && displayMode != .hideAll {
+                Text(transcriptDetailModel.text)
+                    .font(.body)
+                    .foregroundColor(isHighlighted ? .red : .primary)
             }
-            .font(.caption)
-            .foregroundColor(.gray)
+            
+            if displayMode != .hideJapanese && displayMode != .hideAll {
+                HStack {
+                    Text("開始時間: \(transcriptDetailModel.start)秒")
+                    Text("持続時間: \(transcriptDetailModel.duration)秒")
+                }
+                .font(.caption)
+                .foregroundColor(.gray)
+            }
         }
         .padding(.vertical, 4)
         .id(transcriptDetailModel.id)
