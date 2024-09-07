d0f08e5828ab663cab109092a573938fcd7f8f1e
SK3620 <k.n.t11193620@gmail.com>
2024-09-07 23:19:17 +0900

    Clean: ファイルの移動

---
+++ SwiftUIApp/Api/Requests/Post/SignInRequest.swift
@@ -0,0 +1,25 @@
+//
+//  SignInRequest.swift
+//  SwiftUIApp
+//
+//  Created by 鈴木 健太 on 2024/08/15.
+//
+
+import Alamofire
+
+struct SignInRequest: CommonHttpRouter {
+  
+    typealias Response = AuthModel
+    var path: String { return ApiUrl.signInUrl }
+    var method: HTTPMethod { return .post }
+    
+    func body() throws -> Data? {
+        try JSONEncoder().encode(model)
+    }
+    
+    private let model: AuthModel
+    
+    init(model: AuthModel) {
+        self.model = model
+    }
+}
