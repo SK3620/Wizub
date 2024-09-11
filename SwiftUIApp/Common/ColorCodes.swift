f208cc166572cb44d26e91c93f54d8e40a9186c5
SK3620 <k.n.t11193620@gmail.com>
2024-09-11 23:19:00 +0900

    Update: カラーコードに新たな色を追加

--- SwiftUIApp/Common/ColorCodes.swift
+++ SwiftUIApp/Common/ColorCodes.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 enum ColorCodes {
     case primary
+    case primary2
+    case primaryBlack
+    case primaryGray
+    case primaryGray2
+    case primaryLightGray
     case success
     case failure
     case background
@@ -21,6 +26,16 @@ extension ColorCodes {
         switch self {
         case .primary:
             return Color(red: 0.3, green: 0.0, blue: 0.5)
+        case .primary2:
+            return Color(red: 0.95, green: 0.95, blue: 1.0)
+        case .primaryBlack:
+            return Color.black
+        case .primaryGray:
+            return Color.gray
+        case .primaryGray2:
+            return Color(red: 0.45, green: 0.45, blue: 0.45)
+        case .primaryLightGray:
+            return Color(red: 0.9, green: 0.9, blue: 0.9)
         case .success:
             return Color.green
         case .failure:
