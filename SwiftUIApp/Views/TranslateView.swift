d1ff1dec9620b33e70e250df32136aa4b12f57b5
SK3620 <k.n.t11193620@gmail.com>
2024-09-12 09:35:16 +0900

    Add: 翻訳リスト表示画面にて、選択中の字幕がない場合に表示する文言を追加

--- SwiftUIApp/Views/TranslateView.swift
+++ SwiftUIApp/Views/TranslateView.swift
@@ -87,29 +87,43 @@ struct SubtitleList: View {
     var subtitles: [SubtitleModel.SubtitleDetailModel]
     
     var body: some View {
-        List(subtitles) {
-            Text($0.enSubtitle)
-            .padding([.vertical, .horizontal], 16)
-            // 以下は区切り線を端から端まで伸ばす
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .listRowSeparator(.hidden)
-            .listRowInsets(.init(top:0,leading:0,bottom:0,trailing:0))
-            .overlay(
-                Rectangle()
-                    .frame(height: 1)
-                    .foregroundColor(.gray.opacity(0.3)),
-                alignment: .bottom
+        ZStack {
+            List(subtitles) {
+                Text($0.enSubtitle)
+                    .padding([.vertical, .horizontal], 16)
+                // 以下は区切り線を端から端まで伸ばす
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .listRowSeparator(.hidden)
+                    .listRowInsets(.init(top:0,leading:0,bottom:0,trailing:0))
+                    .overlay(
+                        Rectangle()
+                            .frame(height: 1)
+                            .foregroundColor(.gray.opacity(0.3)),
+                        alignment: .bottom
+                    )
+            }
+            .listStyle(.inset)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .background(
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(Color.gray, lineWidth: 0)
             )
+            .shadow(color: .gray, radius: 1)
+            
+            if subtitles.isEmpty {
+                VStack {
+                    Spacer()
+                    Text("選択中の字幕がありません")
+                        .font(.headline)
+                        .foregroundColor(ColorCodes.primaryGray.color())
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
         }
-        .listStyle(.inset)
-        .clipShape(RoundedRectangle(cornerRadius: 16))
-        .background(
-            RoundedRectangle(cornerRadius: 16)
-                .stroke(Color.gray, lineWidth: 0)
-        )
-        .shadow(color: .gray, radius: 1)
     }
 }
+
 
 // 翻訳ボタン
 struct TranslateButton: View {
