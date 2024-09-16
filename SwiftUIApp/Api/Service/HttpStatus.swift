10a35ad46c36418bb6f3ea3823ff25b94c09a075
SK3620 <k.n.t11193620@gmail.com>
2024-09-16 22:01:00 +0900

    Update: ステータスコードからHTTPエラーの種類を返すメソッドを定義

--- SwiftUIApp/Api/Service/HttpStatus.swift
+++ SwiftUIApp/Api/Service/HttpStatus.swift
@@ -7,84 +7,84 @@
 
 import Foundation
 
-enum HttpStatus: Int {
-        
+enum HttpErrorStatus: Int {
+    
     // MARK: - Informational
-//    case continueCode = 100
-//    case switchingProtocols = 101
-//    case processing = 102
-
+    //    case continueCode = 100
+    //    case switchingProtocols = 101
+    //    case processing = 102
+    
     // MARK: - Success
-    case ok = 200
-    case created = 201
-    case accepted = 202
-//    case nonAuthoritativeInformation = 203
-//    case noContent = 204
-//    case resetContent = 205
-//    case partialContent = 206
-//    case multiStatus = 207
-//    case alreadyReported = 208
-//    case imUsed = 226
-
+    //    case ok = 200
+    //    case created = 201
+    //    case accepted = 202
+    //    case nonAuthoritativeInformation = 203
+    //    case noContent = 204
+    //    case resetContent = 205
+    //    case partialContent = 206
+    //    case multiStatus = 207
+    //    case alreadyReported = 208
+    //    case imUsed = 226
+    
     // MARK: - Redirection
-//    case multipleChoices = 300
-//    case movedPermanently = 301
-//    case found = 302
-//    case seeOther = 303
-//    case notModified = 304
-//    case useProxy = 305
-//    case switchProxy = 306
-//    case temporaryRedirect = 307
-//    case permanentRedirect = 308
-
+    //    case multipleChoices = 300
+    //    case movedPermanently = 301
+    //    case found = 302
+    //    case seeOther = 303
+    //    case notModified = 304
+    //    case useProxy = 305
+    //    case switchProxy = 306
+    //    case temporaryRedirect = 307
+    //    case permanentRedirect = 308
+    
     // MARK: - Client Error
     case badRequest = 400
     case unauthorized = 401
-//    case paymentRequired = 402
+    //    case paymentRequired = 402
     case forbidden = 403
-//    case notFound = 404
+    case notFound = 404
     case methodNotAllowed = 405
     case notAcceptable = 406
-//    case proxyAuthenticationRequired = 407
+    //    case proxyAuthenticationRequired = 407
     case requestTimeOut = 408
-//    case conflict = 409
-//    case gone = 410
-//    case lengthRequired = 411
-//    case preconditionFailed = 412
-//    case payloadTooLarge = 413
-//    case uriTooLong = 414
-//    case unsupportedMediaType = 415
-//    case rangeNotSatisfiable = 416
-//    case expectationFailed = 417
-//    case teapod = 418
-//    case misdirectedRequest = 421
-//    case unprocessableEntity = 422
-//    case locked = 423
-//    case failedDependency = 424
-//    case tooEarly = 425
-//    case upgradeRequired = 426
-//    case preconditionRequired = 428
-//    case tooManyRequests = 429
-//    case requestHeaderFieldsTooLarge = 431
-//    case unavailableForLegalReasons = 451
-
+    //    case conflict = 409
+    //    case gone = 410
+    //    case lengthRequired = 411
+    //    case preconditionFailed = 412
+    //    case payloadTooLarge = 413
+    //    case uriTooLong = 414
+    //    case unsupportedMediaType = 415
+    //    case rangeNotSatisfiable = 416
+    //    case expectationFailed = 417
+    //    case teapod = 418
+    //    case misdirectedRequest = 421
+    //    case unprocessableEntity = 422
+    //    case locked = 423
+    //    case failedDependency = 424
+    //    case tooEarly = 425
+    //    case upgradeRequired = 426
+    //    case preconditionRequired = 428
+    //    case tooManyRequests = 429
+    //    case requestHeaderFieldsTooLarge = 431
+    //    case unavailableForLegalReasons = 451
+    
     // MARK: - Server Error
     case internalServerError = 500
-//    case notImplemented = 501
-//    case badGateway = 502
-//    case serviceUnavailable = 503
-//    case gatewayTimeOut = 504
-//    case httpVersionNotSupported = 505
-//    case variantAlsoNegotiates = 506
-//    case insufficientStorage = 507
-//    case loopDetected = 508
-//    case notExtended = 510
-//    case networkAuthenticationRequired = 511
-
+    //    case notImplemented = 501
+    //    case badGateway = 502
+    //    case serviceUnavailable = 503
+    //    case gatewayTimeOut = 504
+    //    case httpVersionNotSupported = 505
+    //    case variantAlsoNegotiates = 506
+    //    case insufficientStorage = 507
+    //    case loopDetected = 508
+    //    case notExtended = 510
+    //    case networkAuthenticationRequired = 511
+    
     // MARK: - unknown
     case unknown = 999
-
-
+    
+    
     // MARK: - Categories of HTTP status codes
     enum Category {
         /// 100-199
@@ -100,23 +100,56 @@ enum HttpStatus: Int {
         /// Not between 100-599
         case unknown
     }
+    
+    struct HttpErrorMessage {
+        var message: String
+        var detail: String
+        
+        init(message: String = "不明なエラーが発生しました。", detail: String = "Unknown Error Ocurred. Detail error message is empty") {
+            self.message = message
+            self.detail = detail
+        }
+    }
 }
 
-
-
-extension HttpStatus {
-      
+extension HttpErrorStatus {
+          
     // MARK: - Initializer, parameter code: The HTTP response code
     init(code: Int) {
         // 引数rawValueに指定した値に対応する要素がない場合はnil(.unknown)
-        if let validStatus = HttpStatus(rawValue: code){
+        if let validStatus = HttpErrorStatus(rawValue: code){
             self = validStatus
         } else {
             self = .unknown
         }
     }
-
+    
+    // MARK: - Public, get a kind of HTTP error
+    func getHttpError(httpErrorMessage: HttpErrorMessage) -> HttpError {
+        switch self {
+        case .badRequest:
+            return HttpError.badRequest(httpErrorMessage)
+        case .unauthorized:
+            return HttpError.unauthorized(httpErrorMessage)
+        case .forbidden:
+            return HttpError.forbidden(httpErrorMessage)
+        case .notFound:
+            return HttpError.notFound(httpErrorMessage)
+        case .methodNotAllowed:
+            return HttpError.methodNotAllowed(httpErrorMessage)
+        case .notAcceptable:
+            return HttpError.notAcceptable(httpErrorMessage)
+        case .requestTimeOut:
+            return HttpError.requestTimeOut(httpErrorMessage)
+        case .internalServerError:
+            return HttpError.serverError(httpErrorMessage)
+        case .unknown:
+            return HttpError.unknown(httpErrorMessage)
+        }
+    }
+    
     // MARK: - The category the status code belongs to
+    /*
     var category: Category {
         let code = self.rawValue
         if code >= 100 && code < 200 {
@@ -138,19 +171,13 @@ extension HttpStatus {
             return .unknown
         }
     }
+     */
     
     // MARK: - Commonly seen status code + Unknown Error
+    /*
     var commonStatus: HttpError {
         let status = self
         switch status {
-            // Success
-        case .ok:
-            break
-        case .created:
-            break
-        case .accepted:
-            break
-            // Client Error
         case .badRequest:
             return HttpError.badRequest
         case .unauthorized:
@@ -159,6 +186,8 @@ extension HttpStatus {
             return HttpError.forbidden
         case .methodNotAllowed:
             return HttpError.methodNotAllowed
+        case .notFound:
+            return HttpError.notFound
         case .notAcceptable:
             return HttpError.notAcceptable
         case .requestTimeOut:
@@ -170,7 +199,6 @@ extension HttpStatus {
         case .unknown:
             return HttpError.unknown
         }
-        
-        return HttpError.unknown
     }
+     */
 }
