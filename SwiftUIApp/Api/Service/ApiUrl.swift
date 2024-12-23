cebde7c25bffc5a399266fa57b4c0349e91cc776
SK3620 <k.n.t11193620@gmail.com>
2024-12-23 23:36:59 +0900

    Change: 各APIのエンドポイントURLを変更

--- SwiftUIApp/Api/Service/ApiUrl.swift
+++ SwiftUIApp/Api/Service/ApiUrl.swift
@@ -19,43 +19,43 @@ struct ApiUrl {
     static let getTrialUserInfo = "api/get_trial_user_info"
     
     // サインアップ post
-    static let signUpUrl = "api/sign_up"
+    static let signUpUrl = "api/auth/sign_up"
 
     // サインイン post
-    static let signInUrl = "api/sign_in"
+    static let signInUrl = "api/auth/sign_in"
     
     // Email重複チェック post
-    static let checkEmailUrl = "api/check_email"
+    static let checkEmailUrl = "api/auth/check_email"
     
     // アカウント削除 delete
-    static let deleteAccout = "api/delete_account"
+    static let deleteAccout = "api/auth/delete_account"
     
     // YouTube動画検索 get
-    static let searchVideos = "api/search"
+    static let searchVideos = "api/videos/search"
     
     // DBに保存したYouTube動画取得 get
-    static let getSavedVideos = "api/get_saved_videos"
+    static let getSavedVideos = "api/videos/saved"
         
     // DBに保存したYouTube動画削除 delete
-    static let deleteSavedVideos = "api/delete_saved_videos"
+    static let deleteSavedVideos = "api/videos"
     
     // DBに保存した字幕情報取得
-    static let getSavedSubtitles = "api/get_saved_subtitles"
+    static let getSavedSubtitles = "api/subtitles/saved"
     
     // 選択した動画の字幕情報取得 get
-    static let getSubtitles = "api/get_subtitles"
+    static let getSubtitles = "api/subtitles"
     
     // 字幕翻訳 get
-    static let translateSubtitles = "api/translate_subtitles"
+    static let translateSubtitles = "api/subtitles/translate"
     
     // 動画情報と字幕の保存 post
-    static let storeSubtitles = "api/store_subtitles"
+    static let storeSubtitles = "api/subtitles"
     
     // 動画がすでに保存済みか否か
-    static let checkVideoAlreadySaved = "api/check_video_already_saved"
+    static let checkVideoAlreadySaved = "api/videos/check"
     
     // 字幕更新 put
-    static let updateSubtitles = "api/update_subtitles"
+    static let updateSubtitles = "api/subtitles"
 
     // なし
     static let none = ""
