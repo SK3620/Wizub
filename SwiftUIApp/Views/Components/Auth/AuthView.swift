0c81ed24d8a0bc779ad8085d47d22d84859b7a26
SK3620 <k.n.t11193620@gmail.com>
2025-01-09 14:14:03 +0900

    Fix: 認証画面のメールアドレス入力欄で先頭文字が自動で大文字になることを防ぐ

--- SwiftUIApp/Views/Components/Auth/AuthView.swift
+++ SwiftUIApp/Views/Components/Auth/AuthView.swift
@@ -13,7 +13,7 @@ struct AuthView: View {
     
     // 親ビューで管理されるフォーカス状態をバインド 現在のフォーカス状態の読み書き可能
     @FocusState.Binding var focusedState: Bool
- 
+    
     private var segmentType: AuthSegmentType {
         return authViewModel.authSegmentType
     }
@@ -23,25 +23,40 @@ struct AuthView: View {
             VStack {
                 if segmentType == .signUpSegment {
                     // ユーザー名入力欄
-                    AuthTextField(title: "UserName", textValue: $authViewModel.userName, errorValue: authViewModel.userNameError)
-                        .focused($focusedState)
-                        .onChange(of: authViewModel.userName) { oldValue, newValue in
-                            // 17文字以上＆スペースは切り捨て
-                            authViewModel.userName = newValue.limited()
-                        }
+                    AuthTextField(
+                        title: "UserName",
+                        textValue: $authViewModel.userName,
+                        errorValue: authViewModel.userNameError
+                    )
+                    .focused($focusedState)
+                    .onChange(of: authViewModel.userName) { oldValue, newValue in
+                        // 17文字以上＆スペースは切り捨て
+                        authViewModel.userName = newValue.limited()
+                    }
                 }
                 
                 // Eメール入力欄
-                AuthTextField(title: "Email", textValue: $authViewModel.email, errorValue: authViewModel.emailError, keyboardType: .emailAddress)
-                    .focused($focusedState)
+                AuthTextField(
+                    title: "Email",
+                    textValue: $authViewModel.email,
+                    errorValue: authViewModel.emailError,
+                    keyboardType: .emailAddress,
+                    autocapitalization: .never
+                )
+                .focused($focusedState)
                 
                 // パスワード入力欄
-                AuthTextField(title: "Password", textValue: $authViewModel.password, errorValue: authViewModel.passwordError, isSecured: true)
-                    .focused($focusedState)
-                    .onChange(of: authViewModel.password) { oldValue, newValue in
-                        // 17文字以上＆スペースは切り捨て
-                        authViewModel.password = newValue.limited()
-                    }
+                AuthTextField(
+                    title: "Password",
+                    textValue: $authViewModel.password,
+                    errorValue: authViewModel.passwordError,
+                    isSecured: true
+                )
+                .focused($focusedState)
+                .onChange(of: authViewModel.password) { oldValue, newValue in
+                    // 17文字以上＆スペースは切り捨て
+                    authViewModel.password = newValue.limited()
+                }
                 
                 // サインイン/サインアップボタン
                 AuthButton(authViewModel: authViewModel)
@@ -57,7 +72,7 @@ struct AuthView: View {
         }
         .background(.white)
         .clipShape(RoundedRectangle(cornerRadius: 35))
-//        .shadow(color: .gray.opacity(0.7), radius: 5)
+        //        .shadow(color: .gray.opacity(0.7), radius: 5)
         .padding(.horizontal)
     }
 }
